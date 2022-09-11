8dd503e3e0569ea9f45af14a15e82febd8366b94
Angel Linares <angel1sma3l@iMac.local>
2022-09-11 13:18:05 -0400

    improve Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,26 +1,46 @@
-import { useRef } from "react";
 import styled from "styled-components";
 
 const Btn = styled.button`
-  width: ${(props) => props.width ?? "200px"};
+  min-width: ${(props) => props.width ?? "150px"};
   background-color: ${(props) => props.bgColor ?? "transparent"};
-  border: 0.5px solid white;
-  height: 50px;
-  border-radius: 25px;
-  color: ${(props) => props.color ?? "white"};
-  font-size: 1.5rem;
-  cursor: pointer;
+  border: ${(props) =>
+    props.borderColor ? "1px solid " + props.borderColor : "none"};
+  height: 40px;
+  border-radius: 22px;
+  color: ${(props) =>
+    props.disabled ? "lightgray" : props.color ? props.color : "white"};
+  font-size: 1.3rem;
+  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
   transition: background-color 500ms ease-out, color 800ms ease;
 
   &:hover {
-    background-color: white;
-    color: black;
+    background-color: ${(props) => props.hoverBg ?? "aquamarine"};
+    color: ${(props) => props.hoverColor ?? "black"};
   }
 `;
 
-const Button = ({ title = "Submit", onClick }) => {
+const Button = ({
+  title = "Submit",
+  borderColor,
+  onClick,
+  type = "button",
+  hoverColor,
+  hoverBg,
+  color,
+  bgColor,
+  disabled,
+}) => {
   return (
-    <Btn onClick={onClick} type="button">
+    <Btn
+      disabled={disabled}
+      onClick={onClick}
+      type={type}
+      hoverBg={hoverBg}
+      hoverColor={hoverColor}
+      bgColor={bgColor}
+      color={color}
+      borderColor={borderColor}
+    >
       {title}
     </Btn>
   );
